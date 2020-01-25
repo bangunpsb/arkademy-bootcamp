36a0956917267e3ef86e403e5de9ea423e9c4c99
Bangun Pasaribu <50303571+bangunpsb@users.noreply.github.com>
2020-01-25 21:53:25 +0700

    Add files via upload

---
+++ 6-a.php
@@ -0,0 +1,31 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "arkademy_bootcamp");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>Chasier</th>
+            <th>Product</th>
+            <th>Category</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $sql = mysqli_query($koneksi, "SELECT product.id_chasier, product.name AS nmp, product.id_category, product.price ,chasier.id,chasier.name AS nmk,category.id,category.name AS nmc from product, category, chasier where product.id_chasier=chasier.id and product.id_category=category.id") ?>
+        <?php while ($data = $sql->fetch_assoc()) { ?>
+            <tr>
+                <td><?= $data['nmk']; ?></td>
+                <td><?= $data['nmp']; ?></td>
+                <td><?= $data['price']; ?></td>
+                <td> <?= $data['nmc']; ?></td>
+            </tr>
+        <?php } ?>
+
+    </tbody>
+</table>
